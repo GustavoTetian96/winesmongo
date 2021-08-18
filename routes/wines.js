262fff10b3f7e256192a37beb58bd9553f391bb5
Gustavo <gusgcast@espol.edu.ec>
2021-08-18 02:01:26 -0500

    base de datos NoSql con mongoDB verificando con postman

---
+++ routes/wines.js
@@ -0,0 +1,69 @@
+var mongo = require('mongodb');
+var Server = mongo.Server;
+var Db = mongo.Db;
+var BSON = mongo.BSONPure;
+var server = new Server('127.0.0.1', 27017, {auto_reconnect: true});
+db = new Db('wine', server);
+
+
+db.open(function(err, db) {
+    if(!err) {
+        console.log("Connected to 'winedb' database");
+        db.collection('wines', {strict:true}, function(err, collection) {
+            if (err) {
+                console.log("The 'wines' collection doesn't exist. Creating it with sample data...");
+                populateDB();
+            }
+        });
+    }
+});
+
+
+exports.findById = function(req, res) {
+    var id = req.params.id;
+    console.log('Retrieving wine: ' + id);
+    db.collection('wines') 
+      .findOne({'_id':new mongo.ObjectID(id)}, function(err, item) {
+            res.send(item);
+        });
+};
+
+
+exports.findAll = function(req, res) {
+    db.collection('wines', function(err, collection) {
+        collection.find().toArray(function(err, items) {
+            res.send(items);
+        });
+    });
+};
+
+
+exports.addWine = function(req, res) {
+    var wine = req.body;
+    console.log('Adding wine: ' + JSON.stringify(wine));
+    db.collection('wines', function(err, collection) {
+        collection.insert(wine, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('Success: ' + JSON.stringify(result[0]));
+                res.send(result[0]);
+            }
+        });
+    });
+}
+
+
+exports.deleteWine = function(req, res) {
+    var id = req.params.id;
+    console.log('Deleting wine: ' + id);
+    db.collection('wines')
+        .remove({'_id':new mongo.ObjectID(id)}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred - ' + err});
+            } else {
+                console.log('' + result + ' document(s) deleted');
+                res.send(req.body);
+            }
+        });
+}
